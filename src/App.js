582272ac2efe23fa6311e7160ab1530bd24ea736
alex2rive3 <alex2rive3@gmail.com>
2023-01-24 14:57:08 -0300

    peticion a la api de pokemon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [result, setResult] = useState([])
+  const handleClick = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
+      .then(response => {
+        return response.json();
+      }).then(response => {
+        setResult(response.results);
+        console.log(response);
+      }).catch(err => {
+        console.log(err);
+      });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>Realizar peticion</button>
+      <ul>
+        {result.map((item, i) => <li key={i}>{item.name}</li>)}
+      </ul>
     </div>
   );
 }
